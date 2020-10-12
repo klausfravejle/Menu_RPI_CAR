9d81ea8cf8ace12601192360bf2a68717c3410de
Klaus Holm <klausfravejle@gmail.com>
2020-10-12 15:32:23 +0200

    setup.py created.

--- menu_rpi.py
+++ menu_rpi.py
@@ -1,48 +1,44 @@
 #! /usr/bin/env python
-import GPIO_TEST as GPIO
-import time
+# Enable this on Raspberry
+# import RPi.GPIO as GPIO
+
 import random
+import time
+import tkinter as tk
 from threading import Thread
-from PIL import Image, ImageTk
 import cv2
+from PIL import Image, ImageTk
+
+# Disable this on Raspberry
+import GPIO_TEST as GPIO
+
+# import menu_rpi_support
 
 # ------------------------GPIO Mode ----------------------------
 # choose BCM or BOARD
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 # ------------------------GPIO PINS ----------------------------
-
-TRIG = 17
-ECHO = 27
-TRIG1 = 18
-ECHO1 = 28
-led = 22
-m11 = 16
-m12 = 12
-m21 = 21
-m22 = 20
+# BCM / BOARD Setup pins
+# https://www.google.com/url?sa=i&url=https%3A%2F%2Fdocs.microsoft.com%2Fen-us%2Fwindows%2Fiot-core%2Flearn-about-hardware%2Fpinmappings%2Fpinmappingsrpi&psig=AOvVaw1jc-PY9cHE5bM75SeY_a4N&ust=1602577275042000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOjYitzPruwCFQAAAAAdAAAAABAJ
+TRIG = 4  # pin 7
+ECHO = 17  # pin 11
+TRIG1 = 27  # pin 13
+ECHO1 = 22  # pin 15
+led = 18  # pin 12
+gpio_right = 5  # pin 29
+gpio_back = 6  # pin 31
+gpio_left = 13  # pin 33
+gpio_forward = 26  # pin 37
 
 # Start ------------------------cv2 webcam settings ----------------------------
 width, height = 600, 450
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+
 # End --------------------------cv2 webcam settings ----------------------------
-
-suitauto = False
-stopauto = 1
-try:
-    import Tkinter as tk
-except ImportError:
-    import tkinter as tk
-
-try:
-    import ttk
-    py3 = False
-except ImportError:
-    import tkinter.ttk as ttk
-    py3 = True
-
-import menu_rpi_support
 
 
 def vp_start_gui():
@@ -50,25 +46,30 @@ def vp_start_gui():
     global val, w, root
     root = tk.Tk()
     top = Toplevel1(root)
-    menu_rpi_support.init(root, top)
+    init(root, top)
     root.mainloop()
-
     w = None
 
 
-def create_Toplevel1(rt, *args, **kwargs):
+def init(top, gui, *args, **kwargs):
+    global w, top_level, root
+    w = gui
+    top_level = top
+    root = top
+
+def create_toplevel1(rt, *args, **kwargs):
     # Starting point when module is imported by another module.
-    # Correct form of call: 'create_Toplevel1(root, *args, **kwargs)'
+    # Correct form of call: 'create_toplevel1(root, *args, **kwargs)'
     global w, w_win, root
     # rt = root
     root = rt
     w = tk.Toplevel(root)
     top = Toplevel1(w)
-    menu_rpi_support.init(w, top, *args, **kwargs)
+    init(w, top, *args, **kwargs)
     return (w, top)
 
 
-def destroy_Toplevel1():
+def destroy_toplevel1():
     global w
     w.destroy()
     w = None
@@ -109,167 +110,180 @@ class Toplevel1:
         self.Frame1.configure(borderwidth="2")
         self.Frame1.configure(relief="groove")
         self.Frame1.configure(background="#d9d9d9")
-        # ----------------------Webcam-START -----------------------
-
         webset()
-        # ----------------------Webcam - END -----------------------
 
         # ----------------------Forward button-----------------------
-        self.forwardbutton = tk.Button(top)
-        self.forwardbutton.place(relx=0.423, rely=0.667, height=42, width=80)
-        self.forwardbutton.configure(activebackground="#ececec")
-        self.forwardbutton.configure(activeforeground="#000000")
-        self.forwardbutton.configure(background="#d9d9d9")
-        self.forwardbutton.configure(command=forward)
-        self.forwardbutton.configure(disabledforeground="#a3a3a3")
-        self.forwardbutton.configure(foreground="#000000")
-        self.forwardbutton.configure(highlightbackground="#d9d9d9")
-        self.forwardbutton.configure(highlightcolor="black")
-        self.forwardbutton.configure(pady="0")
-        self.forwardbutton.configure(text='''Forward''')
+        self.forward_button = tk.Button(top)
+        self.forward_button.place(relx=0.423, rely=0.667, height=42, width=80)
+        self.forward_button.configure(activebackground="#ececec")
+        self.forward_button.configure(activeforeground="#000000")
+        self.forward_button.configure(background="#d9d9d9")
+        self.forward_button.configure(command=forward)
+        self.forward_button.configure(disabledforeground="#a3a3a3")
+        self.forward_button.configure(foreground="#000000")
+        self.forward_button.configure(highlightbackground="#d9d9d9")
+        self.forward_button.configure(highlightcolor="black")
+        self.forward_button.configure(pady="0")
+        self.forward_button.configure(text='''Forward''')
         root.bind('w', forward)
 
         # ----------------------Right button-----------------------
-        self.rightbutton = tk.Button(top)
-        self.rightbutton.place(relx=0.567, rely=0.778, height=42, width=80)
-        self.rightbutton.configure(activebackground="#ececec")
-        self.rightbutton.configure(activeforeground="#000000")
-        self.rightbutton.configure(background="#d9d9d9")
-        self.rightbutton.configure(command=right)
-        self.rightbutton.configure(disabledforeground="#a3a3a3")
-        self.rightbutton.configure(foreground="#000000")
-        self.rightbutton.configure(highlightbackground="#d9d9d9")
-        self.rightbutton.configure(highlightcolor="black")
-        self.rightbutton.configure(pady="0")
-        self.rightbutton.configure(text='''Right''')
+        self.right_button = tk.Button(top)
+        self.right_button.place(relx=0.567, rely=0.778, height=42, width=80)
+        self.right_button.configure(activebackground="#ececec")
+        self.right_button.configure(activeforeground="#000000")
+        self.right_button.configure(background="#d9d9d9")
+        self.right_button.configure(command=right)
+        self.right_button.configure(disabledforeground="#a3a3a3")
+        self.right_button.configure(foreground="#000000")
+        self.right_button.configure(highlightbackground="#d9d9d9")
+        self.right_button.configure(highlightcolor="black")
+        self.right_button.configure(pady="0")
+        self.right_button.configure(text='''Right''')
         root.bind('d', right)
 
         # ----------------------Left button-----------------------
-        self.leftbutton = tk.Button(top)
-        self.leftbutton.place(relx=0.283, rely=0.778, height=42, width=80)
-        self.leftbutton.configure(activebackground="#ececec")
-        self.leftbutton.configure(activeforeground="#000000")
-        self.leftbutton.configure(background="#d9d9d9")
-        self.leftbutton.configure(command=left)
-        self.leftbutton.configure(disabledforeground="#a3a3a3")
-        self.leftbutton.configure(foreground="#000000")
-        self.leftbutton.configure(highlightbackground="#d9d9d9")
-        self.leftbutton.configure(highlightcolor="black")
-        self.leftbutton.configure(pady="0")
-        self.leftbutton.configure(text='''Left''')
+        self.left_button = tk.Button(top)
+        self.left_button.place(relx=0.283, rely=0.778, height=42, width=80)
+        self.left_button.configure(activebackground="#ececec")
+        self.left_button.configure(activeforeground="#000000")
+        self.left_button.configure(background="#d9d9d9")
+        self.left_button.configure(command=left)
+        self.left_button.configure(disabledforeground="#a3a3a3")
+        self.left_button.configure(foreground="#000000")
+        self.left_button.configure(highlightbackground="#d9d9d9")
+        self.left_button.configure(highlightcolor="black")
+        self.left_button.configure(pady="0")
+        self.left_button.configure(text='''Left''')
         root.bind('a', left)
 
         # ----------------------Back button-----------------------
-        self.backbutton = tk.Button(top)
-        self.backbutton.place(relx=0.423, rely=0.889, height=42, width=80)
-        self.backbutton.configure(activebackground="#ececec")
-        self.backbutton.configure(activeforeground="#000000")
-        self.backbutton.configure(background="#d9d9d9")
-        self.backbutton.configure(command=back)
-        self.backbutton.configure(disabledforeground="#a3a3a3")
-        self.backbutton.configure(foreground="#000000")
-        self.backbutton.configure(highlightbackground="#d9d9d9")
-        self.backbutton.configure(highlightcolor="black")
-        self.backbutton.configure(pady="0")
-        self.backbutton.configure(text='''Back''')
+        self.back_button = tk.Button(top)
+        self.back_button.place(relx=0.423, rely=0.889, height=42, width=80)
+        self.back_button.configure(activebackground="#ececec")
+        self.back_button.configure(activeforeground="#000000")
+        self.back_button.configure(background="#d9d9d9")
+        self.back_button.configure(command=back)
+        self.back_button.configure(disabledforeground="#a3a3a3")
+        self.back_button.configure(foreground="#000000")
+        self.back_button.configure(highlightbackground="#d9d9d9")
+        self.back_button.configure(highlightcolor="black")
+        self.back_button.configure(pady="0")
+        self.back_button.configure(text='''Back''')
         root.bind('s', back)
 
         # ----------------------Auto button-----------------------
-        self.autobutton = tk.Button(top)
-        self.autobutton.place(relx=0.067, rely=0.667, height=42, width=54)
-        self.autobutton.configure(activebackground="#ececec")
-        self.autobutton.configure(activeforeground="#000000")
-        self.autobutton.configure(background="#d9d9d9")
-        self.autobutton.configure(command=automode)
-        self.autobutton.configure(disabledforeground="#a3a3a3")
-        self.autobutton.configure(foreground="#000000")
-        self.autobutton.configure(highlightbackground="#d9d9d9")
-        self.autobutton.configure(highlightcolor="black")
-        self.autobutton.configure(pady="0")
-        self.autobutton.configure(text='''Auto''')
+        self.auto_button = tk.Button(top)
+        self.auto_button.place(relx=0.067, rely=0.667, height=42, width=54)
+        self.auto_button.configure(activebackground="#ececec")
+        self.auto_button.configure(activeforeground="#000000")
+        self.auto_button.configure(background="#d9d9d9")
+        self.auto_button.configure(command=automode)
+        self.auto_button.configure(disabledforeground="#a3a3a3")
+        self.auto_button.configure(foreground="#000000")
+        self.auto_button.configure(highlightbackground="#d9d9d9")
+        self.auto_button.configure(highlightcolor="black")
+        self.auto_button.configure(pady="0")
+        self.auto_button.configure(text="Auto")
+
 
 # ----------------------Init GPIO-----------------------
-def GPIO_init():
+
+
+def gpio_init():
     GPIO.setup(TRIG, GPIO.OUT)
     GPIO.setup(ECHO, GPIO.IN)
     GPIO.setup(TRIG1, GPIO.OUT)
     GPIO.setup(ECHO1, GPIO.IN)
     GPIO.setup(led, GPIO.OUT)
-    GPIO.setup(m11, GPIO.OUT)
-    GPIO.setup(m12, GPIO.OUT)
-    GPIO.setup(m21, GPIO.OUT)
-    GPIO.setup(m22, GPIO.OUT)
-GPIO_init()
+    GPIO.setup(gpio_right, GPIO.OUT)
+    GPIO.setup(gpio_back, GPIO.OUT)
+    GPIO.setup(gpio_left, GPIO.OUT)
+    GPIO.setup(gpio_forward, GPIO.OUT)
+
+
+gpio_init()
+
 
 def show_frame():
-        _, frame = cap.read()
-        frame = cv2.flip(frame, 1)
-        cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
-        img = Image.fromarray(cv2image)
-        imgtk = ImageTk.PhotoImage(image=img)
-        lmain.imgtk = imgtk
-        lmain.configure(image=imgtk)
-        lmain.after(10, show_frame)
+    _, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+    img = Image.fromarray(cv2image)
+    imgtk = ImageTk.PhotoImage(image=img)
+    lmain.imgtk = imgtk
+    lmain.configure(image=imgtk)
+    lmain.after(10, show_frame)
+
+
 # ----------------------END show webcam function--------------------
 
 def forward(_event=None):
-    GPIO.output(m11, 1)
-    GPIO.output(m12, 0)
-    GPIO.output(m21, 1)
-    GPIO.output(m22, 0)
+    GPIO.output(gpio_right, 0)
+    GPIO.output(gpio_back, 0)
+    GPIO.output(gpio_left, 0)
+    GPIO.output(gpio_forward, 1)
     print("Car forward")
 
 
 def back(_event=None):
-    GPIO.output(m11, 0)
-    GPIO.output(m12, 1)
-    GPIO.output(m21, 0)
-    GPIO.output(m22, 1)
+    GPIO.output(gpio_right, 0)
+    GPIO.output(gpio_back, 1)
+    GPIO.output(gpio_left, 0)
+    GPIO.output(gpio_forward, 0)
     print("Car back")
 
 
 def right(_event=None):
-    GPIO.output(m11, 1)
-    GPIO.output(m12, 0)
-    GPIO.output(m21, 0)
-    GPIO.output(m22, 0)
+    GPIO.output(gpio_right, 1)
+    GPIO.output(gpio_back, 0)
+    GPIO.output(gpio_left, 0)
+    GPIO.output(gpio_forward, 1)
     print("Car right")
 
 
 def left(_event=None):
-    GPIO.output(m11, 0)
-    GPIO.output(m12, 0)
-    GPIO.output(m21, 1)
-    GPIO.output(m22, 0)
+    GPIO.output(gpio_right, 0)
+    GPIO.output(gpio_back, 0)
+    GPIO.output(gpio_left, 1)
+    GPIO.output(gpio_forward, 0)
     print("Car left")
 
 
 def stop():
     print("Car stop")
-    GPIO.output(m11, 0)
-    GPIO.output(m12, 0)
-    GPIO.output(m21, 0)
-    GPIO.output(m22, 0)
+    GPIO.output(gpio_right, 0)
+    GPIO.output(gpio_back, 0)
+    GPIO.output(gpio_left, 0)
+    GPIO.output(gpio_forward, 0)
 
 
 def automode():
     if __name__ == '__main__':
-            t1 = Thread(target=automodeon)
-            t1.setDaemon(True)
-            t1.start()
+        t1 = Thread(target=automodeon)
+        t1.setDaemon(True)
+        t1.start()
 
+
+# ----------------------flips True / False -----------------------
+def flip(suitauto):
+    return not suitauto
+
+
+# ------Automodeon -  defines how the car drives on auto-----------------------
+suitauto = True
 
 
 def automodeon():
+    global suitauto
+    suitauto = flip(suitauto)
+
     if suitauto is True:
-        print("Autostop")
+        print("Automode disabled!")
     else:
-        leftt = 0
-        rightt = 0
-        while True:
-            # count = 0
-            # i = 0
-            avgDistance = 0
+        print("Automode enabled!")
+        while suitauto is False:
+            avgdistance = 0
             for i in range(5):
                 GPIO.output(TRIG, False)  # Set TRIG as LOW
                 time.sleep(0.1)  # Delay
@@ -278,31 +292,29 @@ def automodeon():
                 time.sleep(0.00001)  # Delay of 0.00001 seconds
                 GPIO.output(TRIG, False)  # Set TRIG as LOW
 
-                while GPIO.input(ECHO) == 0:  # Check whether the ECHO is LOW
-                    GPIO.output(led, False)
+                # while GPIO.input(ECHO) == 0:  # Check whether the ECHO is LOW
+                #    GPIO.output(led, False)
                 pulse_start = time.time()
 
-                while GPIO.input(ECHO) == 1:  # Check whether the ECHO is HIGH
-                    GPIO.output(led, False)
+                # while GPIO.input(ECHO) == 1:  # Check whether the ECHO is HIGH
+                #    GPIO.output(led, False)
                 pulse_end = time.time()
                 pulse_duration = pulse_end - pulse_start  # time to get back the pulse to sensor
 
                 distance = pulse_duration * 17150  # Multiply pulse duration by 17150 (34300/2) to get distance
-
 
                 # ----------------------------- For testing on windows Start ---------------------------
                 distance = random.randint(1, 32)
                 distance = round(distance, 2)  # Round to two decimal points
                 # ----------------------------- For testing on windows End  ---------------------------
 
-
-                avgDistance = avgDistance + distance
-                # print("Average distance cm =" + str(avgDistance))
-            avgDistance = avgDistance / 5
+                avgdistance = avgdistance + distance
+                # print("Average distance cm =" + str(avgdistance))
+            avgdistance = avgdistance / 5
 
             flag = 0
 
-            if avgDistance < 15:  # Check whether the distance is within 15 cm range
+            if avgdistance < 15:  # Check whether the distance is within 15 cm range
                 stop()
                 # time.sleep(1)
                 time.sleep(0.5)
@@ -316,12 +328,11 @@ def automodeon():
 
                 if (count % 3 == 1) & (flag == 0):
                     right()
-                    rightt = rightt + 1
                     flag = 1
 
                 else:
                     left()
-                    leftt = leftt + 1
+
                     flag = 0
                     # time.sleep(1.5)
                     time.sleep(0.5)
@@ -334,6 +345,5 @@ def automodeon():
                 flag = 0
 
 
-if __name__ == '__main__':
-    vp_start_gui()
+vp_start_gui()
 GPIO.cleanup()
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Menu_RPI_CAR_git',
+    version='1',
+    packages=[''],
+    url='',
+    license='',
+    author='Klaus',
+    author_email='klausfravejle@gmail.com',
+    description='Self driving RPI RC car with webcam'
+)
